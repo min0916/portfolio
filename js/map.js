b9f5bdc4fa319ee2bce673994698611389676f4c
mingyeom <sdh180502@sdh.hs.kr>
2020-05-25 02:34:20 +0900

    2020_05_25_AM:2:34

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
-        center: new kakao.maps.LatLng(37.5251952,126.8539493), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+        center: new kakao.maps.LatLng(37.520601,126.84039), // 지도의 중심좌표
+        level: 5 // 지도의 확대 레벨
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
