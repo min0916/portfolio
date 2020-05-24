40ac84855f112340bd8e60993accd32a641ff71f
mingyeom <sdh180502@sdh.hs.kr>
2020-05-24 19:37:33 +0900

    카카오맵 추가, 반응형 헤더 작업, 반응형 스크립트 추가

--- js/script.js
+++ js/script.js
@@ -1,20 +1,53 @@
-$(window).on('scroll', function () {
-    const scrollTop = $(window).scrollTop();
-    console.log(scrollTop);
-    if(scrollTop > 200) {
-        $('html').addClass('scrolled')
-    }
-    else {
-        $('html').removeClass('scrolled')
-    }
+$(function () {
 
-    if(scrollTop > 500) {
-        $('#header').fadeIn(400);
-    }
-    else{
-        $('#header').fadeOut(400);
-    }
+    portfolio.init()
 })
-$(".about").click(function() {
-    var scrollPosition = $($(this).attr("data-target")).offset().top;
-})
+
+const portfolio = {
+    init: function () {
+        portfolio.getScroll()
+        portfolio.handleHeader()
+        portfolio.scrollTo()
+        portfolio.openMenu()
+    },
+    getScroll: function () {
+        $(window).on('scroll', function () {
+            const scrollTop = $(window).scrollTop();
+            if (scrollTop > 200) {
+                $('html').addClass('scrolled')
+            } else {
+                $('html').removeClass('scrolled')
+            }
+
+        })
+    },
+    handleHeader: function () {
+        $(window).on('scroll', function () {
+            const scrollTop = $(window).scrollTop();
+            if (scrollTop > 500) {
+                $('#header').fadeIn(400);
+            } else {
+                $('#header').fadeOut(400);
+            }
+        })
+    },
+    scrollTo: function () {
+        $('#header nav a').on('click', function () {
+            const dataTarget = $(this).data('target');
+            const offsetTop = $(dataTarget).offset().top;
+            $('html, body').animate({
+                scrollTop: offsetTop - 60
+            })
+        })
+    },
+    openMenu: function () {
+        $('.btn-menu').on('click', function () {
+            $('html').toggleClass('open-manu');
+
+        });
+        $('#header .screen').on('click', function () {
+            $('html').removeClass('open-manu');
+        });
+    }
+}
+
